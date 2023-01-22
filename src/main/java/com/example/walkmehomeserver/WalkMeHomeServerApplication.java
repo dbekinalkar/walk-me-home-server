97ee95f1a34a14d9d3f412925f5c5a48bda1ceee
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-01-21 23:23:06 -0500

    first commit

---
+++ src/main/java/com/example/walkmehomeserver/WalkMeHomeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.walkmehomeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkMeHomeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkMeHomeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/walkmehomeserver/WalkMeHomeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walkmehomeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkMeHomeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
